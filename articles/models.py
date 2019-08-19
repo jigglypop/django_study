e78356f442cf98b8b15a8641f915e11629c6750b
jigglypop <ydh2244@gmail.com>
2019-08-19 15:01:03 +0900

    README 정리

--- articles/models.py
+++ articles/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-
 # Create your models here.
 # 1. 모델(스키마) 정의
 # 스키마 정의
@@ -12,14 +11,12 @@ class Article(models.Model):
 
     title = models.CharField(max_length=10)
     content = models.TextField()
-    
     # DateTimeField
     # auto_now_add : 생성시 자동으로 입력
     # auto_now : 수정시마다 자동으로 저장
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
     def __str__(self):
         return f'{self.id} : {self.title}'
 
@@ -28,3 +25,5 @@ class Article(models.Model):
 # makemigrations : migration 파일 생성
 #                : DB 설계도 작성
 # migrate : migration 파일 DB 변경
+
+
