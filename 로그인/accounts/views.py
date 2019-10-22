e349d6d1d87d8a4e04dde57d1e2a43f85e82f53b
jigglypop <ydh2244@gmail.com>
2019-10-22 19:02:56 +0900

    1022

---
+++ 로그인/accounts/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render,redirect
+from .forms import UserCreationForm,CustomUserCreationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.forms import AuthenticationForm
+
+# Create your views here.
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('articles:index')
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request,user)
+            return redirect('articles:index')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/form.html', context)
+
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('articles:index')
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect(request.GET.get('next') or 'articles:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form':form
+    }
+    return render(request,'accounts/login.html',context)
+
+def logout(request):
+    auth_logout(request)
+    return redirect('articles:index')
