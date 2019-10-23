e99e41b3ba0253183f710caf7d436a247f9fd3dc
jigglypop <ydh2244@gmail.com>
2019-10-23 17:21:18 +0900

    1023

--- 좋아요/accounts/views.py
+++ 좋아요/accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from .forms import UserCreationForm,CustomUserCreationForm
+from .forms import CustomUserCreationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.forms import AuthenticationForm
